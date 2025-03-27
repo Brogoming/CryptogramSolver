d960539d7ceca7e320996e65843036602c000c3c
Dakota Gullicksen <kotag@outlook.com>
2025-03-26 21:07:14 -0500

    setting up tkinter

--- tkinker/main.py
+++ tkinker/main.py
@@ -1,29 +1,39 @@
-import tkinter as tk
+from tkinter import *
+from tkinter.ttk import Combobox
 
-class App(tk.Frame):
-    def __init__(self, master):
-        super().__init__(master)
-        self.pack()
 
-        self.entrythingy = tk.Entry()
-        self.entrythingy.pack()
+def select(event):
+    selected_item = combo_box.get()
+    print(selected_item)
 
-        # Create the application variable.
-        self.contents = tk.StringVar()
-        # Set it to some value.
-        self.contents.set("this is a variable")
-        # Tell the entry widget to watch this variable.
-        self.entrythingy["textvariable"] = self.contents
+def action():
+    print("button clicked")
 
-        # Define a callback for when the user hits return.
-        # It prints the current value of the variable.
-        self.entrythingy.bind('<Key-Return>',
-                             self.print_contents)
+root = Tk()
+root.title("Cryptogram Solver 1.6.0")
+root.geometry('500x400')
+Label(root, text='Model:').grid(row=0, column=0)
+Label(root, text='Model Name:').grid(row=0, column=2)
+Label(root, text='Number of Sentences:').grid(row=1, column=0)
+Label(root, text='Randomness:').grid(row=1, column=2)
+Label(root, text='N Estimators:').grid(row=2, column=0)
+Entry(root).grid(row=0, column=1)  # model type
 
-    def print_contents(self, event):
-        print("Hi. The current entry content is:",
-              self.contents.get())
+# Create Widgets
+combo_box = Combobox(root, values=["RandomForestClassifier", "LogisticRegression", "LinearRegression"])
+combo_box.set("RandomForestClassifier")
+combo_box.bind("<<ComboboxSelected>>", select)
+modelName = Entry(root)
+numSent = Entry(root)
+randomness = Entry(root)
+nEstimators = Entry(root)
+trainModelButton = Button(root, text='Train Model', width=30, bg="blue", foreground="white", command=action)
 
-root = tk.Tk()
-myapp = App(root)
-myapp.mainloop()
+combo_box.grid(row=0, column=1)  # model type
+modelName.grid(row=0, column=3)  # model name
+numSent.grid(row=1, column=1)  # number of sentences to train
+randomness.grid(row=1, column=3)  # randomness
+nEstimators.grid(row=2, column=1)  # number of estimators
+trainModelButton.grid(row=2, column=2, columnspan=2)  # train model activation
+
+root.mainloop()
